7a1251248886949e9a5d816f5a990057cf87a169
dima <rundoom1337@gmail.com>
2022-02-06 02:08:07 +0400

    У монстров теперь есть базовый урон Новый контент

--- src/main/kotlin/Generator.kt
+++ src/main/kotlin/Generator.kt
@@ -4,15 +4,16 @@ import java.nio.charset.Charset
 
 fun generateCreatures(){
     val isAnyEnabled = Creatures.values().any { it.isEnabled }
-    val out = "Name,Description,Image,level,hp\n" + ",,,,\n" +
+    val out = "Name,Description,Image,level,hp,Basedmg\n" + ",,,,,\n" +
     Creatures.values().filter { !isAnyEnabled || it.isEnabled }.joinToString("\n") {
         it.showName + "," +
-                '"' + "(do): " + it.activeEffect.br + "<br>" +
+                '"' + if(it.activeEffect != null){"(do): " + it.activeEffect.br + "<br>"} else {""} +
                 if(it.passiveEffect != null){"(pass): " + it.passiveEffect.br + "<br>"} else {""} +
                 it.level.hand + ": " + it.handEffect.br + '"' + "," +
                 '"' + "images\\cover\\" + it.image + '"' + "," +
                 it.level + "," +
-                it.hp
+                it.hp + "," +
+                it.damage
     }
 
    File("nandeck\\creatures\\creatures.csv").writeText(out, Charset.forName("Windows-1251"))
