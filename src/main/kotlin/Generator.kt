0dc02f3f7b0e386d8227ecd8e148c13be2aa933d
dima <rundoom1337@gmail.com>
2022-02-07 21:02:05 +0400

    Новый формат существ

--- src/main/kotlin/Generator.kt
+++ src/main/kotlin/Generator.kt
@@ -3,13 +3,13 @@ import java.nio.charset.Charset
 
 
 fun generateCreatures(){
-    val isAnyEnabled = Creatures.values().any { it.isEnabled }
+    val isAnyEnabled = Creatures_new.values().any { it.isEnabled }
     val out = "Name,Description,Image,level,hp,Basedmg\n" + ",,,,,\n" +
-    Creatures.values().filter { !isAnyEnabled || it.isEnabled }.joinToString("\n") {
+            Creatures_new.values().filter { !isAnyEnabled || it.isEnabled }.joinToString("\n") {
         it.showName + "," +
                 '"' + if(it.activeEffect != null){"(do): " + it.activeEffect.br + "<br>"} else {""} +
                 if(it.passiveEffect != null){"(pass): " + it.passiveEffect.br + "<br>"} else {""} +
-                it.level.hand + ": " + it.handEffect.br + '"' + "," +
+                if(it.handEffect != null) {it.level.hand + ": " + it.handEffect.br} else {""} + '"' + "," +
                 '"' + "images\\cover\\" + it.image + '"' + "," +
                 it.level + "," +
                 it.hp + "," +
